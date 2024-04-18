1a5907910b666d45a93234b8cd75ffcc9b041c55
avi4803 <167286701+avi4803@users.noreply.github.com>
2024-04-18 21:51:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// let a;1
+// let b;
+// let c;
+// a = window.prompt("Input value of a");
+// a = Number(a)
+// b = window.prompt("Input value of b");
+// b = Number(b)
+// c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+// console.log(c);
